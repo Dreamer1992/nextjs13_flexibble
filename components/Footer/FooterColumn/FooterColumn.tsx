960a77e1011ef6421ffbe7960822973081cd1132
Ganiev Davlet <davlet@routeamgroup.com>
2023-09-22 11:52:32 +0300

    feat: init grafbase

--- components/Footer/FooterColumn/FooterColumn.tsx
+++ components/Footer/FooterColumn/FooterColumn.tsx
@@ -8,6 +8,7 @@ interface IProps {
 const FooterColumn = ({ title, links }: IProps) => (
   <div className="footer_column">
     <h4 className="font-semibold">{title}</h4>
+    
     <ul className="flex flex-col gap-2 font-normal">
       {links.map((link) => (
         <Link href="/" key={link}>
